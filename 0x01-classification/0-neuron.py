d0c7b71ebd6659b919b44ab77c9411572a76d30b
eyadev <46867140+eyadev@users.noreply.github.com>
2021-03-26 22:01:34 +0100

    Update 0-neuron.py

--- 0x01-classification/0-neuron.py
+++ 0x01-classification/0-neuron.py
@@ -1,13 +1,10 @@
+#!/usr/bin/env python3
 import numpy as np
 
-Neuron = __import__('0-neuron').Neuron
-
-lib_train = np.load('Bureau/ML/data/Binary_Train.npz')
-X_3D, Y = lib_train['X'], lib_train['Y']
-X = X_3D.reshape((X_3D.shape[0], -1)).T
 
 class Neuron:
 
+    
     def __init__(self, nx):
         if type(nx) != int:
             raise TypeError("nx must be an integer")
@@ -17,12 +14,3 @@ class Neuron:
         self.w=np.random.randn(1,2)*0.01 
         self.b=0
         self.A=0   
-
-np.random.seed(0)
-neuron = Neuron(X.shape[0])
-print(neuron.W)
-print(neuron.W.shape)
-print(neuron.b)
-print(neuron.A)
-neuron.A = 10
-print(neuron.A)
