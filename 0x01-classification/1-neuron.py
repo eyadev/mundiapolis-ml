0029cd661ba28e9b2b2baad58bd60ee17e205b58
eyadev <46867140+eyadev@users.noreply.github.com>
2021-03-26 22:25:44 +0100

    Add files via upload

---
+++ 0x01-classification/1-neuron.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+class Neuron:
+
+
+    def __init__(self, nx):
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        elif nx < 1:
+            raise ValueError('nx must be a positive integer')
+        else:
+            self.__W = np.random.randn(1,2)*0.01 
+            self.__b = 0
+            self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
